e41bea5a378aced0e0d206a1ef116457abe63753
rafaelmum <rcosta@mum.edu>
2017-10-26 08:38:36 -0500

    Updated Dashboard service.
    Signed-off-by: rafaelmum <rcosta@mum.edu>

--- routes/userprofile.js
+++ routes/userprofile.js
@@ -10,14 +10,10 @@ var url = 'mongodb://127.0.0.1/rentmovie';
 
 /* GET user. */
 router.get('/user/:username', function(req, res, next) {
-  console.log('req.params.username: ' + req.params.username);
-
   User.findOne({username: req.params.username}, function(err, userReturned) {
     if (err) {
       return console.error(err);
     }
-
-    console.log('userReturned: ' + userReturned);
 
     res.end(JSON.stringify(userReturned));
   });
@@ -25,8 +21,6 @@ router.get('/user/:username', function(req, res, next) {
 
 /* GET movie given. */
 router.get('/movieGiven/:username', function(req, res, next) {
-  console.log('req.params.username: ' + req.params.username);
-  
   var resultArray= [];
 
   MongoClient.connect(url).then((db)=> {
@@ -34,7 +28,6 @@ router.get('/movieGiven/:username', function(req, res, next) {
     data.forEach(function(doc, err){
       resultArray.push(doc);
     }, function(){
-      console.log('resultArray: ' + resultArray);
       res.send(resultArray);
       db.close();
     })
@@ -45,14 +38,11 @@ router.get('/movieGiven/:username', function(req, res, next) {
 
 /* GET movie received. */
 router.get('/movieReceived/:username', function(req, res, next) {
-  console.log('req.params.username: ' + req.params.username);
-  
   var resultArray= [];
 
   MongoClient.connect(url).then((db)=> {
     let data = db.collection('movies').find({newowner: req.params.username});
     data.forEach(function(doc, err){
-      console.log('resultArray: ' + resultArray);
       resultArray.push(doc);
     }, function(){
       res.send(resultArray);
